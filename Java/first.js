311e43e5b26e5b1eef7cddfc5c1b80c7184c04fe
Shell <naveed.ahmed@sacl.com>
2024-08-28 23:38:50 +0500

    add files and folders

---
+++ Java/first.js
@@ -0,0 +1,86 @@
+// console.log("Apna College+");
+// console.log("Hello World");
+
+// let age=25;
+// console.log(age);
+
+// let price=25.9;
+// let y=null;
+// let fullName="Naveed";
+// let fullname="Mustafa";
+
+// console.log(fullName);
+// console.log(price);
+// console.log(y);
+// console.log(fullname);
+
+// {
+//     let name="Minal";
+//     console.log(name);
+// }
+
+// {
+//     let name="Minha";
+//     console.log(name);
+// }
+// typeof fullName
+
+// const student={
+// fullName: "Naveed Ahmed",
+// age:40,
+// ispass:true
+
+// };
+// console.log(student["age"]);
+
+// student["age"]=student["age"]+1;
+// console.log(typeof student["age"]);
+
+// console.log(typeof student["fullName"]);
+// console.log(typeof student);
+
+// console.log(student);
+// console.log(typeof student["age"]);
+// console.log(student["fullName"]);
+
+// const student01={
+//     fullName: "Minal Ahmed",
+//     age:8,
+//     ispass:true
+    
+//     };
+
+//     console.log(typeof student01["age"]);
+//     console.log(typeof fullName);
+//     console.log(student01["age"]);
+
+//     console.log(student01["age"]);
+//     student01[age]=student01[age]+1;
+//     console.log(student01["age",+1]);
+
+let a=4
+let b=5
+a**=2
+console.log("a+b=",a+b);
+
+console.log("b-a=",b-a);
+console.log(a*b);
+console.log(a/b);
+console.log(b%b);
+console.log(a**5);
+
+
+console.log(a);
+console.log(a!==b);
+
+condition1 = a>b;
+condition2 = a=b;
+console.log(condition1&&condition2);
+
+condition1 = a<b;
+condition2 = a=b;
+console.log(condition1&&condition2);
+
+
+
+
